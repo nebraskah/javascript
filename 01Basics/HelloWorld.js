a1b5f8f19d9e4a0de60e6bd68224662a3b1d5423
DOMINGOS1 <domingosm@discovery.co.za>
2023-05-28 22:36:11 +0200

    setups & configs

--- 01Basics/HelloWorld.js
+++ 01Basics/HelloWorld.js
@@ -2,3 +2,8 @@ console.log('---------------');
 console.log('Hello World');
 console.log('Rise & Shine');
 console.log('---------------');
+
+const prompt = require('prompt-sync')();
+let firstName = prompt("Enter name: ");
+let surname = prompt("Enter Surname: ");
+console.log('Welcome',firstName,surname);
